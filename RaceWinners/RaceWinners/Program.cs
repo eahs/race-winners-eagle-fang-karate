0bc1d83e3801b466364b488dc826a7606cc41b9c
Vincent Lauro <vinniehat@gmail.com>
2021-09-24 08:54:33 -0400

    Commented out for new algorithm

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -12,13 +12,35 @@ namespace RaceWinners
             // Asynchronously retrieve the group (class) data
             var data = await ds.GetGroupRanksAsync();
 
-            for (int i = 0; i < data.Count; i++)
-            {
-                // Combine the ranks to print as a list
-                var ranks = String.Join(", ", data[i].Ranks);
-                
-                Console.WriteLine($"{data[i].Name} - [{ranks}]");
-            }
+            // var num_of_students = 0;
+            // var num_of_students2 = 0;
+            //
+            // for (int i = 0; i < data.Count; i++)
+            // {
+            //     for (int x = 0; x < data[i].Ranks.Count; x++)
+            //     {
+            //         if (num_of_students == 0)
+            //         {
+            //             num_of_students++;
+            //         } else num_of_students2++;
+            //     }
+            //     
+            //     if (num_of_students2 > num_of_students) // RAISE the max students that can be counted
+            //     {
+            //         num_of_students2 = 0;
+            //     } else if (num_of_students2 < num_of_students) // This means you have to LOWER the max students that can be counted
+            //     {
+            //         num_of_students = num_of_students2;
+            //         num_of_students2 = 0;
+            //     }
+            // }
+            //
+            // Console.WriteLine(num_of_students);
+            
+            
+            
+            
+            
         }
     }
 }
